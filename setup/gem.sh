7e8e2ecfed34ae712a4a745153c57ff318b64549
Stefan Imhoff <stefan@imhoff.name>
2021-11-28 10:46:42 +0100

    chore(gem): add install script for Ruby gems

---
+++ setup/gem.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+gem install bundler         # Bundler manages an application's dependencies
+gem install git-up          # git command to fetch and rebase all branches
+gem install github-linguist # Language Savant.
+gem install lunchy          # Friendly wrapper around launchctl
+gem install tmuxinator      # Create and manage complex tmux sessions easily.
+
