f9f7fb3a2d867ed6704701f4e9f68d16eaf80a98
Stefan Imhoff <stefan@imhoff.name>
2021-11-28 11:40:27 +0100

    chore(ruby): add install script for Ruby

--- setup/init.sh
+++ setup/init.sh
@@ -10,5 +10,8 @@ source ./npm.sh
 # Installing Python version manager
 source ./python.sh
 
+# Install current version of Ruby
+source ./ruby.sh
+
 # Install global Gems
 source ./gem.sh
---
+++ setup/ruby.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+rbenv install $(rbenv install -l | grep -v - | tail -1)
+rbenv global $(rbenv install -l | grep -v - | tail -1)
+
