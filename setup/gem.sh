d96d7f2bddc400f0ee3399a5c1139782cd2e2a36
Stefan Imhoff <stefan@imhoff.name>
2021-12-11 13:32:04 +0100

    chore(gem): add ruby provider for NeoVim

--- setup/gem.sh
+++ setup/gem.sh
@@ -5,3 +5,4 @@ gem install git-up          # git command to fetch and rebase all branches
 gem install github-linguist # Language Savant.
 gem install lunchy          # Friendly wrapper around launchctl
 gem install tmuxinator      # Create and manage complex tmux sessions easily.
+gem install neovim          # Ruby provider for NeoVim
