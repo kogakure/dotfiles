c69f87a9b12fe1c22977e0d3e2c3ea09c8cd210b
Stefan Imhoff <stefan@imhoff.name>
2021-12-28 11:40:29 +0100

    chore(gem): move to brew-gem for installing ruby gems

--- setup/gem.sh
+++ setup/gem.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-gem install bundler         # Bundler manages an application's dependencies
-gem install git-up          # git command to fetch and rebase all branches
-gem install github-linguist # Language Savant.
-gem install lunchy          # Friendly wrapper around launchctl
-gem install neovim          # Ruby provider for NeoVim
+brew install brew-gem
+
+brew gem install bundler         # Bundler manages an application's dependencies
+brew gem install git-up          # git command to fetch and rebase all branches
+brew gem install github-linguist # Language Savant.
+brew gem install lunchy          # Friendly wrapper around launchctl
+brew gem install neovim          # Ruby provider for NeoVim
