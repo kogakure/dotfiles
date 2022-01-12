045e52d389e243124c9f0e097feb46972cf70947
Stefan Imhoff <stefan@imhoff.name>
2022-01-12 10:23:48 +0100

    chore(vim): add mapping for finding the current file in NvimTree

--- nvim/plugin-config/tree.lua
+++ nvim/plugin-config/tree.lua
@@ -4,10 +4,11 @@
 local status, nvim_tree = pcall(require, 'nvim-tree')
 if (not status) then return end
 
-nvim_tree.setup({})
+nvim_tree.setup()
 
 --- Mappings
 local opts = { noremap = true, silent = true }
 
 vim.api.nvim_set_keymap('n', '<leader>nt',  [[<Cmd>:NvimTreeToggle<CR>]], opts)
+vim.api.nvim_set_keymap('n', '<leader>ntf',  [[<Cmd>:NvimTreeFindFile<CR>]], opts)
 vim.api.nvim_set_keymap('n', '<leader>ntr', [[<Cmd>:NvimTreeRefresh<CR>]], opts)
