b2fb765967e2b8010683c62497260f6ff43cf15a
Stefan Imhoff <stefan@imhoff.name>
2022-02-21 09:22:13 +0100

    chore: uncomment cmp configuration

--- nvim/plugin-config/cmp.lua
+++ nvim/plugin-config/cmp.lua
@@ -6,11 +6,11 @@ if (not status) then return end
 
 cmp.setup({
   auto_select = false,
-  -- snippet = {
-  --   expand = function(args)
-  --     vim.fn["UltiSnips#Anon"](args.body)
-  --   end
-  -- },
+  snippet = {
+    expand = function(args)
+      vim.fn["UltiSnips#Anon"](args.body)
+    end
+  },
   mapping = {
     ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -29,9 +29,9 @@ cmp.setup({
   sources = {
     { name = 'nvim_lsp' },
     { name = 'path' },
-    -- { name = 'ultisnips' },
-    { name = 'buffer', keyword_length = 5 },
+    { name = 'buffer', keyword_length = 3 },
     { name = 'npm', keyword_length = 4 },
+    { name = 'ultisnips' },
   },
   formatting = {
     format = require('lspkind').cmp_format {
