d2aff56b362e1f6df5197de40f4d3c54ffc8e952
Stefan Imhoff <stefan@imhoff.name>
2022-07-12 16:41:06 +0200

    chore(nvim): reformat file

--- nvim/lua/kogakure/treesitter.lua
+++ nvim/lua/kogakure/treesitter.lua
@@ -1,32 +1,32 @@
 -- nvim-treesitter – https://github.com/nvim-treesitter/nvim-treesitter/
 local status_ok, configs = pcall(require, "nvim-treesitter.configs")
 if not status_ok then
-  return
+	return
 end
 
-configs.setup {
-  ensure_installed = "all",
-  sync_install = false,
-  ignore_install = { "" }, -- List of parsers to ignore installing
-  autopairs = {
-    enable = true,
-  },
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-  },
-  indent = { enable = true, disable = { "yaml", "css" } },
-  context_commentstring = {
-    enable = true,
-    enable_autocmd = false,
-  },
-  rainbow = {
-    enable = true,
-    extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
-  },
-  playground = {
-    enable = true,
-  }
-}
+configs.setup({
+	ensure_installed = "all",
+	sync_install = false,
+	ignore_install = { "" }, -- List of parsers to ignore installing
+	autopairs = {
+		enable = true,
+	},
+	highlight = {
+		enable = true, -- false will disable the whole extension
+		disable = { "" }, -- list of language that will be disabled
+		additional_vim_regex_highlighting = true,
+	},
+	indent = { enable = true, disable = { "yaml", "css" } },
+	context_commentstring = {
+		enable = true,
+		enable_autocmd = false,
+	},
+	rainbow = {
+		enable = true,
+		extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+		max_file_lines = nil, -- Do not enable for files with more than n lines, int
+	},
+	playground = {
+		enable = true,
+	},
+})
