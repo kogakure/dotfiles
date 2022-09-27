9903c54ee304770f52e629ba9515488023602f41
Stefan Imhoff <stefan@imhoff.name>
2022-09-27 10:48:40 +0200

    chore(python): add yt-dlp and dependencies

--- setup/python.sh
+++ setup/python.sh
@@ -11,22 +11,27 @@ eval "$(pyenv virtualenv-init -)"
 
 pyenv install 3.10.1
 
-pip install fawkes
 pip install autopep8
-pip install black
-pip install yamllint
-pip install vim-vint
-pip install gitlint
-pip install codespell
-pip install proselint
-pip install flake8
-pip install neovim
 pip install awscli
+pip install black
+pip install certifi
+pip install codespell
+pip install fawkes
+pip install flake8
+pip install gitlint
 pip install jupyter
-pip install jupyterlab
 pip install jupyter_ascending
+pip install jupyterlab
+pip install mutagen
+pip install neovim
 pip install notebook
+pip install proselint
+pip install pycryptodomex
+pip install vim-vint
 pip install voila
+pip install websockets
+pip install yamllint
+pip install yt-dlp
 
 jupyter nbextension install --py --sys-prefix jupyter_ascending
 jupyter nbextension     enable jupyter_ascending --sys-prefix --py
