46c69592b3a8410c548c7723d21cb4410a22fcde
Stefan Imhoff <stefan@imhoff.name>
2022-09-29 11:53:16 +0200

    Revert "chore(nvim): add hologram plugin"
    This reverts commit 4e966190c9fe645a5e1d3d4c5eb15eb0b744af10.

--- nvim/after/plugin/hologram.lua
+++
@@ -1,9 +0,0 @@
--- Hologram – https://github.com/edluffy/hologram.nvim
-local status, hologram = pcall(require, "hologram")
-if not status then
-	return
-end
-
-hologram.setup({
-	auto_display = true,
-})
