6db81cc658015c78ad7b353d18eca0360ae96c68
Stefan Imhoff <stefan@imhoff.name>
2022-09-30 21:45:55 +0200

    fix: failing colorscheme plugins on first load

---
+++ nvim/after/plugin/catppuccin.lua
@@ -0,0 +1,9 @@
+-- Catppuccin Theme – https://github.com/catppuccin/catppuccin
+local status_ok, catppucin = pcall(require, "catppuccin")
+if not status_ok then
+	return
+end
+
+vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
+
+require("catppuccin").setup()
--- nvim/lua/kogakure/colorscheme.lua
+++ nvim/lua/kogakure/colorscheme.lua
@@ -9,14 +9,8 @@ if not status_ok then
 	return
 end
 
--- Catppuccin Theme
-vim.g.catppuccin_flavour = "mocha" -- latte, frappe, macchiato, mocha
-require("catppuccin").setup()
-
--- Set colorscheme and additional colors
 vim.cmd([[
-colorscheme tokyonight-night
-
+silent! colorscheme tokyonight-night
 highlight ColorColumn guibg=#202224
 highlight SpellBad guifg=red
 ]])
