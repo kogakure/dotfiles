fe88e37e986d6c8b1b11d58dd6720e6d3101cd11
Stefan Imhoff <stefan@imhoff.name>
2022-11-06 12:56:35 +0100

    chore(nvim): update Alpha screen commands

--- nvim/after/plugin/alpha.lua
+++ nvim/after/plugin/alpha.lua
@@ -15,14 +15,15 @@ dashboard.section.header.val = {
 }
 
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find Files", ":FzfLua files<CR>"),
+	dashboard.button("f", "  Find Files", ":Telescope find_files<CR>"),
 	dashboard.button("e", "  New File", ":ene <BAR> startinsert<CR>"),
 	dashboard.button("p", "  Find Project", ":Telescope projects<CR>"),
-	dashboard.button("r", "  Recently Used Files", ":FzfLua oldfiles<CR>"),
-	dashboard.button("t", "  Find Text", ":FzfLua live_grep<CR>"),
+	dashboard.button("r", "  Recently Used Files", ":Telescope oldfiles<CR>"),
+	dashboard.button("t", "  Find Text", ":Telescope grep_string<CR>"),
 	dashboard.button("b", "  Browser Bookmarks", ":Telescope bookmarks<CR>"),
 	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua<CR>"),
 	dashboard.button("u", "  Update Plugins", ":PackerUpdate<CR>"),
+	dashboard.button("m", "  Mason (LSP, DAP, Linter, Formatter)", ":Mason<CR>"),
 	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
 
