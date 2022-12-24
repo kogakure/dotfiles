669fac667af9efce176888d969de8072d9f60ac5
Stefan Imhoff <stefan@imhoff.name>
2022-12-24 11:47:10 +0100

    chore(nvim): make bufferline transparent

--- nvim/after/plugin/bufferline.lua
+++ nvim/after/plugin/bufferline.lua
@@ -2,37 +2,186 @@
 require("bufferline").setup({
 	highlights = {
 		fill = {
-			bg = "#282828",
+			bg = "None",
 		},
-		tab_selected = {
-			fg = {
-				attribute = "fg",
-				highlight = "Normal",
-			},
-			bg = {
-				attribute = "bg",
-				highlight = "Normal",
-			},
+		background = {
+			bg = "None",
 		},
 		tab = {
-			fg = {
-				attribute = "fg",
-				highlight = "TabLine",
-			},
-			bg = {
-				attribute = "bg",
-				highlight = "TabLine",
-			},
+			bg = "None",
+		},
+		tab_selected = {
+			bg = "None",
+		},
+		tab_close = {
+			bg = "None",
+		},
+		close_button = {
+			bg = "None",
+		},
+		close_button_visible = {
+			bg = "None",
+		},
+		close_button_selected = {
+			bg = "None",
+		},
+		buffer_visible = {
+			bg = "None",
+		},
+		buffer_selected = {
+			bg = "None",
+			bold = true,
+		},
+		numbers = {
+			bg = "None",
+		},
+		numbers_visible = {
+			bg = "None",
+		},
+		numbers_selected = {
+			bg = "None",
+			bold = true,
+		},
+		diagnostic = {
+			bg = "None",
+		},
+		diagnostic_visible = {
+			bg = "None",
+		},
+		diagnostic_selected = {
+			bg = "None",
+			bold = true,
+		},
+		hint = {
+			bg = "None",
+		},
+		hint_visible = {
+			bg = "None",
+		},
+		hint_selected = {
+			bg = "None",
+			bold = true,
+		},
+		hint_diagnostic = {
+			bg = "None",
+		},
+		hint_diagnostic_visible = {
+			bg = "None",
+		},
+		hint_diagnostic_selected = {
+			bg = "None",
+			bold = true,
+		},
+		info = {
+			bg = "None",
+		},
+		info_visible = {
+			bg = "None",
+		},
+		info_selected = {
+			bg = "None",
+			bold = true,
+		},
+		info_diagnostic = {
+			bg = "None",
+		},
+		info_diagnostic_visible = {
+			bg = "None",
+		},
+		info_diagnostic_selected = {
+			bg = "None",
+			bold = true,
+		},
+		warning = {
+			bg = "None",
+		},
+		warning_visible = {
+			bg = "None",
+		},
+		warning_selected = {
+			bg = "None",
+			bold = true,
+		},
+		warning_diagnostic = {
+			bg = "None",
+		},
+		warning_diagnostic_visible = {
+			bg = "None",
+		},
+		warning_diagnostic_selected = {
+			bg = "None",
+			bold = true,
+		},
+		error = {
+			bg = "None",
+		},
+		error_visible = {
+			bg = "None",
+		},
+		error_selected = {
+			bg = "None",
+			bold = true,
+		},
+		error_diagnostic = {
+			bg = "None",
+		},
+		error_diagnostic_visible = {
+			bg = "None",
+		},
+		error_diagnostic_selected = {
+			bg = "None",
+			bold = true,
+		},
+		modified = {
+			bg = "None",
+		},
+		modified_visible = {
+			bg = "None",
+		},
+		modified_selected = {
+			bg = "None",
+		},
+		duplicate_selected = {
+			bg = "None",
+			italic = true,
+		},
+		duplicate_visible = {
+			bg = "None",
+			italic = true,
+		},
+		duplicate = {
+			bg = "None",
+			italic = true,
+		},
+		separator_selected = {
+			bg = "None",
+		},
+		separator_visible = {
+			bg = "None",
+		},
+		separator = {
+			bg = "None",
 		},
 		indicator_selected = {
-			fg = {
-				attribute = "fg",
-				highlight = "LspDiagnosticsDefaultHint",
-			},
-			bg = {
-				attribute = "bg",
-				highlight = "Normal",
-			},
+			bg = "None",
+		},
+		pick_selected = {
+			bg = "None",
+			bold = true,
+			italic = true,
+		},
+		pick_visible = {
+			bg = "None",
+			bold = true,
+			italic = true,
+		},
+		pick = {
+			bg = "None",
+			bold = true,
+			italic = true,
+		},
+		offset_separator = {
+			bg = "None",
 		},
 	},
 	options = {
