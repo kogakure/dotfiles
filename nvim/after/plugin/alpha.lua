b3078d32b202c7a9a6b74683b2ac3bdb04052bae
Stefan Imhoff <stefan@imhoff.name>
2022-12-25 10:21:54 +0100

    chore(nvim): reduce items on Alpha dashboard

--- nvim/after/plugin/alpha.lua
+++ nvim/after/plugin/alpha.lua
@@ -5,13 +5,11 @@ local dashboard = require("alpha.themes.dashboard")
 dashboard.section.header.val = {}
 
 dashboard.section.buttons.val = {
-	dashboard.button("f", "  Find Files", ":Telescope find_files<CR>"),
 	dashboard.button("e", "  New File", ":ene <BAR> startinsert<CR>"),
+	dashboard.button("f", "  Find Files", ":Telescope find_files<CR>"),
 	dashboard.button("p", "  Find Project", ":Telescope projects<CR>"),
 	dashboard.button("r", "  Recently Used Files", ":Telescope oldfiles<CR>"),
 	dashboard.button("t", "  Find Text", ":Telescope grep_string<CR>"),
-	dashboard.button("b", "  Browser Bookmarks", ":Telescope bookmarks<CR>"),
-	dashboard.button("c", "  Configuration", ":e ~/.config/nvim/init.lua<CR>"),
 	dashboard.button("u", "  Update Plugins", ":Lazy sync<CR>"),
 	dashboard.button("m", "  Mason (LSP, DAP, Linter, Formatter)", ":Mason<CR>"),
 	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
