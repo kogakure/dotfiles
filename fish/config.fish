bdf6e669a16af08aec8c47fd40dc976a64eb39fc
Stefan Imhoff <stefan@imhoff.name>
2022-12-27 12:19:19 +0100

    feat(fish): add starship prompt, asdf version manager and Neovim as default editor

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,18 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Prompt
+starship init fish | source
+
+# asdf Version Manager
+source /usr/local/opt/asdf/libexec/asdf.fish
+
+# Neovim as default editor
+set -U EDITOR nvim
+
+alias v "vim"
+
+if type nvim > /dev/null 2>&1
+  alias vim 'nvim'
+end
