4a8c96e979e398d33538cc26f2564a87dd9d6378
Stefan Imhoff <stefan@imhoff.name>
2023-01-12 17:37:16 +0100

    feat: add function to filter GitHub pull requests for zsh and fish shell

---
+++ fish/functions/ghpr.fish
@@ -0,0 +1,5 @@
+# Search and preview GitHub pull requests
+function ghpr
+  set -l GH_FORCE_TTY 100%
+  gh pr list | fzf --ansi --preview 'GH_FORCE_TTY=100% gh pr view {1}' --preview-window down --header-lines 3 | awk '{print $1}' | xargs gh pr checkout
+end
