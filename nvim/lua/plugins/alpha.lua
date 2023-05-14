6490bac4616cd50caf586e25bccdfec934026601
Stefan Imhoff <stefan@imhoff.name>
2023-05-14 14:56:59 +0200

    fix(nerdfont): broken Symbols in Alpha dashboard

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -13,12 +13,12 @@ return {
     opts.section.buttons.val = {
       opts.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
       opts.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-      opts.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
+      opts.button("r", "󰈞 " .. " Recent files", ":Telescope oldfiles <CR>"),
       opts.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
       opts.button("p", " " .. " Find Project", ":Telescope projects<CR>"),
-      opts.button("s", "勒" .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
-      opts.button("l", "鈴" .. " Lazy (Plugins)", ":Lazy<CR>"),
-      opts.button("m", " " .. " Mason (LSP, DAP, Linter, Formatter)", ":Mason<CR>"),
+      opts.button("s", "󰁯" .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
+      opts.button("l", "󰒲" .. " Lazy (Plugins)", ":Lazy<CR>"),
+      opts.button("m", "󱊈" .. " Mason (LSP, DAP, Linter, Formatter)", ":Mason<CR>"),
       opts.button("q", " " .. " Quit", ":qa<CR>"),
       opts.padding,
     }
