9a7f6402fd6382b1d79a3d10e72be734bdddda7d
Stefan Imhoff <stefan@imhoff.name>
2023-06-04 17:20:44 +0200

    feat(nvim): remap keys for telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -4,10 +4,10 @@ return {
   "telescope.nvim",
   cond = vim.g.vscode == nil,
   keys = {
-    { "<M-b>", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-    { "<C-p>", "<cmd>Telescope find_files hidden=true<cr>", desc = "Find Files (hidden)" },
+    { "<C-p>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
     { "<C-t>", "<cmd>Telescope<cr>", desc = "Telescope" },
-    { "<M-p>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+    { "<M-b>", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+    { "<M-p>", "<cmd>Telescope find_files hidden=true<cr>", desc = "Find Files (hidden)" },
     { "<leader>sg", "<cmd>Telescope live_grep_args<cr>", desc = "Live Grep" },
     { "<leader>tr", "<cmd>Telescope resume<cr>", desc = "Telescope" },
     { "<leader>tu", "<cmd>Telescope undo<cr>", desc = "Undo Tree" },
