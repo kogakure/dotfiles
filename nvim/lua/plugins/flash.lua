b56be6e877aadbb9a8490e690eafe8617f705dc3
Stefan Imhoff <stefan@imhoff.name>
2023-06-24 10:15:46 +0200

    feat(nvim): add flash.nvim plugin

---
+++ nvim/lua/plugins/flash.lua
@@ -0,0 +1,33 @@
+-- flash.nvim
+-- https://github.com/folke/flash.nvim
+return {
+  "folke/flash.nvim",
+  event = "VeryLazy",
+  opts = {},
+  keys = {
+    {
+      "s",
+      mode = { "n", "x", "o" },
+      function()
+        require("flash").jump()
+      end,
+      desc = "Flash",
+    },
+    {
+      "S",
+      mode = { "n", "o", "x" },
+      function()
+        require("flash").treesitter()
+      end,
+      desc = "Flash Treesitter",
+    },
+    {
+      "r",
+      mode = "o",
+      function()
+        require("flash").remote()
+      end,
+      desc = "Remote Flash",
+    },
+  },
+}
