fc3bdea5861c9d3e0d688b9f17546059f4243667
Stefan Imhoff <stefan@imhoff.name>
2023-09-08 10:53:52 +0200

    chore(fish): deactivate automatic MacOS update

--- fish/functions/update.fish
+++ fish/functions/update.fish
@@ -5,5 +5,5 @@ function update --description "Updating Homebrew, Ruby, Python, Node.js, Neovim,
     pip install --upgrade pip
     pip list -o --format columns | cut -d' ' -f1 | xargs -n1 pip install -U
     pnpm update -g
-    sudo softwareupdate -i -a
+    # sudo softwareupdate -i -a
 end
