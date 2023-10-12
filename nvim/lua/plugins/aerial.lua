74ee8caf885aab8f0bb0676166cd61794a7aa655
Stefan Imhoff <stefan@imhoff.name>
2023-10-12 14:13:00 +0200

    feat(nvim): add aerial plugin

---
+++ nvim/lua/plugins/aerial.lua
@@ -0,0 +1,22 @@
+-- aerical.nvim
+-- https://github.com/stevearc/aerial.nvim
+return {
+  "stevearc/aerial.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    require("aerial").setup({
+      on_attach = function(bufnr)
+        -- Jump forwards/backwards with '{' and '}'
+        vim.keymap.set("n", "{", "<cmd>AerialPrev<CR>", { buffer = bufnr })
+        vim.keymap.set("n", "}", "<cmd>AerialNext<CR>", { buffer = bufnr })
+      end,
+    })
+  end,
+  keys = {
+    { "<leader>A", "<cmd>AerialToggle!<CR>", desc = "Aerial" },
+  },
+}
