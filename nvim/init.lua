f3b09a51091a75b6e161986037937e66d0e9a6cc
Stefan Imhoff <stefan@imhoff.name>
2023-10-13 14:28:37 +0200

    feat(nvim): upgrade LazyVim to version 10

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,3 +3,6 @@ require("config.lazy")
 
 -- Custom Highlight
 vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#1F2334" })
+vim.api.nvim_set_hl(0, "DashboardHeader", { fg = "#A8CD76" })
+vim.api.nvim_set_hl(0, "DashboardFooter", { fg = "#A8CD76" })
+vim.api.nvim_set_hl(0, "DashboardIcon", { fg = "#A8CD76" })
--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -3,7 +3,7 @@
 return {
   "neovim/nvim-lspconfig",
   init = function()
-    require("lazyvim.util").on_attach(function(_, buffer)
+    require("lazyvim.util").lsp.on_attach(function(_, buffer)
 	  -- stylua: ignore
       vim.keymap.set("n", "g0", "<cmd>Telescope lsp_document_symbols<cr>", { buffer = buffer, desc = "Document Symbols" })
     end)
