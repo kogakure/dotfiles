66b181cd559b2168273918f94c648fc2ad90dbbd
Stefan Imhoff <stefan@imhoff.name>
2023-11-01 10:46:49 +0100

    refactor(fish): create separate backups for work and home

--- fish/functions/homebrewBackup.fish
+++ fish/functions/homebrewBackup.fish
@@ -1,4 +1,8 @@
 function homebrewBackup --description "Create a snapshot of all currently installed Homebrew, Cask, Mac App Store, and WhaleBrew installations"
-    cd ~/.dotfiles/
-    brew bundle dump --describe -f
+    switch $hostname
+        case mac-mini
+            brew bundle dump --force --describe --file=~/.dotfiles/Brewfile
+        case '*'
+            brew bundle dump --force --describe --file=~/.dotfiles/Brewfile-Work
+    end
 end
--- fish/functions/homebrewRestore.fish
+++ fish/functions/homebrewRestore.fish
@@ -1,3 +1,8 @@
 function homebrewRestore --description "Install all Homebrew, Cask, Mac App Store, and WhaleBrew applications from a snapshot"
-  brew bundle --file ~/.dotfiles/Brewfile
+    switch $hostname
+        case mac-mini
+            brew bundle --file ~/.dotfiles/Brewfile
+        case '*'
+            brew bundle --file ~/.dotfiles/Brewfile-Work
+    end
 end
