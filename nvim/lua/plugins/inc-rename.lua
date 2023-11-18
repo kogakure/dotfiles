9835fe72f7d0664c3bb4ba1da4b1035301da9281
Stefan Imhoff <stefan@imhoff.name>
2023-11-18 14:19:26 +0100

    feat(nvim): add inc-rename.nvim plugin

---
+++ nvim/lua/plugins/inc-rename.lua
@@ -0,0 +1,7 @@
+-- Incremental LSP renaming based on Neovim's command-preview feature.
+-- https://github.com/smjonas/inc-rename.nvim
+return {
+  "smjonas/inc-rename.nvim",
+  cmd = "IncRename",
+  config = true,
+}
