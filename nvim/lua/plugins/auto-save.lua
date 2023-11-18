a2cbc7f3c311c3ff9cd0987f07119cb581ab1e55
Stefan Imhoff <stefan@imhoff.name>
2023-11-18 14:39:18 +0100

    feat(nvim): add auto-save.nvim

---
+++ nvim/lua/plugins/auto-save.lua
@@ -0,0 +1,8 @@
+-- 🧶 Automatically save your changes in NeoVim
+-- https://github.com/pocco81/auto-save.nvim
+return {
+  "Pocco81/auto-save.nvim",
+  keys = {
+    { "<leader>as", "<Cmd>ASToggle<CR>", desc = "Autosave Toggle" },
+  },
+}
