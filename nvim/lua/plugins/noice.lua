996454585f29e3e080fee66594464d18ee7443ea
Stefan Imhoff <stefan@imhoff.name>
2023-11-18 14:19:15 +0100

    feat(nvim): add border to hover and filter out no information messages

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -8,5 +8,18 @@ return {
         enabled = false,
       },
     },
+    routes = {
+      {
+        filter = {
+          event = "notify",
+          find = "No information available",
+        },
+        opts = { skip = true },
+      },
+    },
+    presets = {
+      inc_rename = true,
+      lsp_doc_border = true,
+    },
   },
 }
