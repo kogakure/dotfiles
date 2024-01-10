34f9894e58e0eed56dd1402ce1767a2cc00812dd
Stefan Imhoff <stefan@imhoff.name>
2024-01-10 14:41:35 +0100

    feat(sketchybar): add sample configuration for sketchybar

---
+++ sketchybar/items/cpu.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sketchybar --add item cpu right --set cpu \
+	icon=􀧓 \
+	update_freq=2 \
+	script="$PLUGIN_DIR/cpu.sh"
---
+++ sketchybar/plugins/calendar.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+#
+LC_TIME=de_DE.UTF-8 sketchybar --set $NAME label="$(date +'%a %d.%m.%Y %H:%M')"
