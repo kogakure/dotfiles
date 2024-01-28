855ac8897bc86759bcbacf2ab19d0b6d411b1336
Stefan Imhoff <stefan@imhoff.name>
2024-01-28 17:09:41 +0100

    chore(wezterm): add comments to keybindings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -62,41 +62,36 @@ config.window_padding = {
 
 -- Custom Keybindings
 config.keys = {
-	{ key = "Enter", mods = "CMD", action = wezterm.action.ToggleFullScreen },
-	{ key = "%", mods = "CTRL|ALT|SHIFT", action = wezterm.action({ SendString = "\2%" }) },
-	{ key = ",", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2v" }) },
-	{ key = ",", mods = "CTRL|CMD", action = wezterm.action({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
-	{ key = "1", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\21" }) },
-	{ key = "2", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\22" }) },
-	{ key = "3", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\23" }) },
-	{ key = "4", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\24" }) },
-	{ key = "5", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\25" }) },
-	{ key = "6", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\26" }) },
-	{ key = "7", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\27" }) },
-	{ key = "8", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\28" }) },
-	{ key = "9", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\29" }) },
-	{ key = ";", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2:" }) },
-	{ key = "D", mods = "CTRL|ALT|SHIFT", action = wezterm.action({ SendString = "\2D" }) },
-	{ key = "[", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2p" }) },
-	{ key = "]", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2n" }) },
-	{ key = "d", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2d" }) },
-	{ key = "f", mods = "CTRL|ALT", action = wezterm.action({ SendString = ":Grep\n" }) },
-	{ key = "f", mods = "CTRL|ALT|SHIFT", action = wezterm.action({ SendString = "\2O" }) },
-	{ key = "g", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2G" }) },
-	{ key = "h", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2p" }) },
-	{ key = "i", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2i" }) },
-	{ key = "j", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2s" }) },
-	{ key = "k", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2s" }) },
-	{ key = "l", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2n" }) },
-	{ key = "n", mods = "CTRL|ALT|SHIFT", action = wezterm.action({ SendString = "" }) },
-	{ key = "p", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2\84" }) },
-	{ key = "q", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2d" }) },
-	{ key = "r", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2R" }) },
-	{ key = "s", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2f" }) },
-	{ key = "t", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2c" }) },
-	{ key = "w", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2x" }) },
-	{ key = "y", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2!" }) },
-	{ key = "z", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2z" }) },
+	{ key = "Enter", mods = "CMD", action = wezterm.action.ToggleFullScreen }, -- Toggle fullscreen
+	{ key = ",", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2v" }) }, -- Rename window
+	{ key = "1", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\21" }) }, -- window 1
+	{ key = "2", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\22" }) }, -- window 2
+	{ key = "3", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\23" }) }, -- window 3
+	{ key = "4", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\24" }) }, -- window 4
+	{ key = "5", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\25" }) }, -- window 5
+	{ key = "6", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\26" }) }, -- window 6
+	{ key = "7", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\27" }) }, -- window 7
+	{ key = "8", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\28" }) }, -- window 8
+	{ key = "9", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\29" }) }, -- window 9
+	{ key = ";", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2:" }) }, -- Command Mode
+	{ key = "D", mods = "CTRL|ALT|SHIFT", action = wezterm.action({ SendString = "\2D" }) }, -- Lazydocker
+	{ key = "[", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2p" }) }, -- Previous window
+	{ key = "]", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2n" }) }, -- Next window
+	{ key = "d", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2d" }) }, -- Detach session
+	{ key = "f", mods = "CTRL|ALT", action = wezterm.action({ SendString = ":Grep\n" }) }, -- Telescope Live Grep
+	{ key = "g", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2G" }) }, -- Lazygit
+	{ key = "h", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2p" }) }, -- Previous window
+	{ key = "i", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2i" }) }, -- Cht.sh
+	{ key = "j", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2s" }) }, -- Select windows
+	{ key = "k", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2s" }) }, -- Select windows
+	{ key = "l", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2n" }) }, -- Next window
+	{ key = "p", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2\84" }) }, -- Smart tmux session manager
+	{ key = "q", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2d" }) }, -- Detach session
+	{ key = "r", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2R" }) }, -- Return to last session
+	{ key = "t", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2c" }) }, -- New window
+	{ key = "w", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2x" }) }, -- Close window
+	{ key = "y", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2!" }) }, -- Move pane to new window
+	{ key = "z", mods = "CTRL|ALT", action = wezterm.action({ SendString = "\2z" }) }, -- Zoom into window
 }
 
 return config
