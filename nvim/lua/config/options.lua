802652c14fa7fb4d9c9b1fccb15a6bb2b1ca0338
Stefan Imhoff <gpg@kogakure.8shield.net>
2024-03-07 10:57:58 +0100

    chore(nvim): change root_spec
    This turns of the annoying root detection in mono repositories with multiple package.json

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -25,6 +25,9 @@ vim.opt.iskeyword:append("-") -- Add dashes to words
 vim.opt.wildignore:append({ "*/node_modules/*" }) -- Wildignore
 vim.opt.complete:append({ "i", "k", "s", "kspell" })
 
+-- Only the project root should be the root
+vim.g.root_spec = { ".git" }
+
 -- Undercurl
 vim.cmd([[let &t_Cs = "\e[4:3m"]])
 vim.cmd([[let &t_Ce = "\e[4:0m"]])
