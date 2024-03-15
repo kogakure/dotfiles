d6bba72ad20badf6689c374d8852592cbbec2efd
Stefan Imhoff <gpg@kogakure.8shield.net>
2024-03-15 21:07:03 +0100

    chore(nvim): change model to codellama

--- nvim/lua/plugins/ollama.lua
+++ nvim/lua/plugins/ollama.lua
@@ -3,7 +3,7 @@
 return {
   "David-Kunz/gen.nvim",
   opts = {
-    model = "mistral", -- The default model to use.
+    model = "codellama", -- The default model to use.
     display_mode = "split", -- The display mode. Can be "float" or "split".
     show_prompt = true, -- Shows the Prompt submitted to Ollama.
     show_model = true, -- Displays which model you are using at the beginning of your chat session.
