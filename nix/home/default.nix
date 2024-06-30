9721fbbaef72e9338780cebae910ac224aba82ff
Stefan Imhoff <gpg@kogakure.8shield.net>
2024-06-30 12:32:52 +0200

    chore(nix): add comment

--- nix/home/default.nix
+++ nix/home/default.nix
@@ -24,7 +24,7 @@ in
     # The home.packages option allows you to install Nix packages into your
     # environment.
     home.packages = with pkgs; [
-      nixpkgs-fmt
+      nixpkgs-fmt # Nix code formatter
     ];
   };
 }
