1fe539cda369ac53c0e061aa99299f773c37561a
Stefan Imhoff <gpg@kogakure.8shield.net>
2024-07-10 18:03:45 +0200

    feat(nix): migrate karabiner-elements

--- nix/home/default.nix
+++ nix/home/default.nix
@@ -11,6 +11,7 @@ in
     ./asdf
     ./bat
     ./ctags
+    ./karabiner
     ./lazydocker
     ./lazygit
     ./ripgrep
---
+++ nix/home/karabiner/default.nix
@@ -0,0 +1,11 @@
+{ pkgs, ... }:
+
+{
+  home.file.".config/karabiner/" = {
+    source = ./karabiner;
+  };
+
+  home.packages = [
+    pkgs.karabiner-elements
+  ];
+}
