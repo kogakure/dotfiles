4748fadd896b14a74a7bf900b5f9094ed5e45965
Stefan Imhoff <gpg@kogakure.8shield.net>
2024-07-10 18:13:33 +0200

    chore: remove sketchybar

--- sketchybar/items/cpu.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sketchybar --add item cpu right --set cpu \
-	icon=􀧓 \
-	update_freq=2 \
-	script="$PLUGIN_DIR/cpu.sh"
--- sketchybar/plugins/calendar.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#
-LC_TIME=de_DE.UTF-8 sketchybar --set $NAME label="$(date +'%a %d.%m.%Y %H:%M')"
