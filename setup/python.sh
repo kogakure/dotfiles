2c85f3126eaedec20f4d577f8907502fcd714b1f
Stefan Imhoff <gpg@kogakure.8shield.net>
2024-07-12 20:15:23 +0200

    chore: remove pip and conda environment

--- setup/python.sh
+++ setup/python.sh
@@ -3,8 +3,3 @@
 asdf plugin add python
 asdf install python latest
 asdf global python latest
-
-pip install -r ~/.dotfiles/python/pip-requirements.txt
-
-conda install -n base ipykernel --update-deps --force-reinstall
-conda install --file ~/.dotfiles/python/conda-requirements.txt
