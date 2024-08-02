c33de5d12537b5ff49208bca557008cab09359aa
Stefan Imhoff <gpg@kogakure.8shield.net>
2024-08-02 16:22:42 +0200

    fix(nix): remove line that crashed my Finder

--- darwin/mac-extra.sh
+++ darwin/mac-extra.sh
@@ -1,6 +1,3 @@
 # Icon Settings
 /usr/libexec/PlistBuddy -c "Set DesktopViewSettings:IconViewSettings:iconSize 44" ~/Library/Preferences/com.apple.finder.plist
 /usr/libexec/PlistBuddy -c "Set DesktopViewSettings:IconViewSettings:gridSpacing 60" ~/Library/Preferences/com.apple.finder.plist
-
-# Restart Finder
-killall Finder
