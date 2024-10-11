e69099a2f78a19e3d250cead2790ec0cd9d0e65a
Stefan Imhoff <gpg@kogakure.8shield.net>
2024-10-11 15:32:53 +0200

    chore: backup

--- config/nvim/lua/plugins/highlight-undo.lua
+++ config/nvim/lua/plugins/highlight-undo.lua
@@ -1,15 +1,16 @@
 -- highlight-undo.nvim
 -- https://github.com/tzachar/highlight-undo.nvim
 return {
-  "tzachar/highlight-undo.nvim",
-  config = function()
-    require("highlight-undo").setup({
-      hlgroup = "HighlightUndo",
-      duration = 300,
-      keymaps = {
-        { "n", "u", "undo", {} },
-        { "n", "<C-r>", "redo", {} },
-      },
-    })
-  end,
+	"tzachar/highlight-undo.nvim",
+	enabled = false,
+	config = function()
+		require("highlight-undo").setup({
+			hlgroup = "HighlightUndo",
+			duration = 300,
+			keymaps = {
+				{ "n", "u", "undo", {} },
+				{ "n", "<C-r>", "redo", {} },
+			},
+		})
+	end,
 }
