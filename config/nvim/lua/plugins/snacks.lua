9a1b15ef12ddf9ae8d59650de503e900a907f59d
Stefan Imhoff <gpg@kogakure.8shield.net>
2025-03-16 16:24:51 +0100

    chore(nvim): remove projects from dashboard

--- config/nvim/lua/plugins/snacks.lua
+++ config/nvim/lua/plugins/snacks.lua
@@ -33,7 +33,6 @@ return {
 				{ section = "header" },
 				{ icon = " ", title = "Keymaps", section = "keys", indent = 2, padding = 1 },
 				{ icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-				{ icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
 				{ section = "startup" },
 			},
 		},
