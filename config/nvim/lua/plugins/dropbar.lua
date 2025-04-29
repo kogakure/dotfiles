9bb3b9ff479f0f35039f34ac93fd889e80d8cd17
Stefan Imhoff <gpg@kogakure.8shield.net>
2025-04-29 11:18:28 +0200

    fix(nvim): replace archived plugin with new plugin

---
+++ config/nvim/lua/plugins/dropbar.lua
@@ -0,0 +1,15 @@
+-- IDE-like breadcrumbs, out of the box
+-- https://github.com/Bekaboo/dropbar.nvim
+return {
+	"Bekaboo/dropbar.nvim",
+	dependencies = {
+		"nvim-telescope/telescope-fzf-native.nvim",
+		build = "make",
+	},
+	config = function()
+		local dropbar_api = require("dropbar.api")
+		vim.keymap.set("n", "<Leader>\\", dropbar_api.pick, { desc = "Pick symbols in winbar" })
+		vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
+		vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+	end,
+}
