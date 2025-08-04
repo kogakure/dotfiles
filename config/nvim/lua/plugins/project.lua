04ce5e58e5fb187459777c001482a1b2b99ff813
Stefan Imhoff <gpg@kogakure.8shield.net>
2025-08-04 14:15:55 +0200

    chore(nvim): updates to maps

--- config/nvim/lua/plugins/project.lua
+++ config/nvim/lua/plugins/project.lua
@@ -1,24 +1,24 @@
 -- The superior project management solution for neovim
 -- https://github.com/ahmedkhalf/project.nvim
 return {
-  "ahmedkhalf/project.nvim",
-  lazy = false,
-  keys = {
+	"ahmedkhalf/project.nvim",
+	lazy = false,
+	keys = {
 	-- stylua: ignore
     { "<leader>p", ":Telescope projects<cr>", desc = "Search Projects" },
-  },
-  config = function()
-    require("project_nvim").setup({
-      active = true,
-      on_config_done = nil,
-      manual_mode = false,
-      detection_methods = { "pattern" },
-      patterns = { ".git", "Makefile", ".gitignore", "package.json", "!node_modules" },
-      show_hidden = false,
-      silent_chdir = true,
-      ignore_lsp = {},
-      datapath = vim.fn.stdpath("data"),
-    })
-    require("telescope").load_extension("projects")
-  end,
+	},
+	config = function()
+		require("project_nvim").setup({
+			active = true,
+			on_config_done = nil,
+			manual_mode = false,
+			detection_methods = { "pattern" },
+			patterns = { ".git", "Makefile", ".gitignore", "package.json", "!node_modules" },
+			show_hidden = true,
+			silent_chdir = true,
+			ignore_lsp = {},
+			datapath = vim.fn.stdpath("data"),
+		})
+		require("telescope").load_extension("projects")
+	end,
 }
