01f8ec1368b5f48734ddf79aecc7e23be7323e1c
Stefan Imhoff <gpg@kogakure.8shield.net>
2025-08-22 21:32:33 +0200

    feat(nvim): add typr plugin

---
+++ config/nvim/lua/plugins/typr.lua
@@ -0,0 +1,8 @@
+-- Most Beautiful Typing practice plugin for Neovim with dashboard
+-- https://github.com/nvzone/typr
+return {
+	"nvzone/typr",
+	dependencies = "nvzone/volt",
+	opts = {},
+	cmd = { "Typr", "TyprStats" },
+}
