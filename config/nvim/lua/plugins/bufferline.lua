55905a6eb2e83cc63b975c45fc44488e1ce8c731
Stefan Imhoff <gpg@kogakure.8shield.net>
2025-08-27 13:29:47 +0200

    fix(nvim): disable bufferline

--- config/nvim/lua/plugins/bufferline.lua
+++ config/nvim/lua/plugins/bufferline.lua
@@ -1,17 +1,18 @@
 -- A snazzy bufferline for Neovim
 -- https://github.com/akinsho/bufferline.nvim
 return {
-  "akinsho/bufferline.nvim",
-  dependencies = "nvim-tree/nvim-web-devicons",
-  keys = {
-    { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
-    { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
-  },
-  opts = {
-    options = {
-      mode = "tabs",
-      show_buffer_close_icons = false,
-      show_close_icon = false,
-    },
-  },
+	"akinsho/bufferline.nvim",
+	enabled = false,
+	dependencies = "nvim-tree/nvim-web-devicons",
+	keys = {
+		{ "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+		{ "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+	},
+	opts = {
+		options = {
+			mode = "tabs",
+			show_buffer_close_icons = false,
+			show_close_icon = false,
+		},
+	},
 }
