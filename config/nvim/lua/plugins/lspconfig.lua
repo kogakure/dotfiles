a33c698a225f62593a8f6be9b35fc1c6283b9be0
Stefan Imhoff <gpg@kogakure.8shield.net>
2025-10-31 15:00:11 +0100

    fix(nvim): issue with lazyvim.util api

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -2,16 +2,14 @@
 -- https://github.com/neovim/nvim-lspconfig
 return {
 	"neovim/nvim-lspconfig",
-	init = function()
-		require("lazyvim.util").lsp.on_attach(function(_, buffer)
-      -- stylua: ignore
-      vim.keymap.set("n", "g0", "<cmd>Telescope lsp_document_symbols<cr>", { buffer = buffer, desc = "Document Symbols" })
-			vim.keymap.set("n", "cc", "<cmd>lua vim.lsp.buf.code_action()<cr>", { buffer = buffer, desc = "Code Action" })
-		end)
-	end,
 	opts = {
 		inlay_hints = { enabled = false },
 		servers = {
+			["*"] = {
+				keys = {
+					{ "g0", "<cmd>Telescope lsp_document_symbols<cr>", desc = "Document Symbols" },
+				},
+			},
 			astro = {},
 			cssls = {},
 			cssmodules_ls = {},
