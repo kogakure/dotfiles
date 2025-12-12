f557f38afd75a5c1ffa71392c2feacc28f6c5dd8
Stefan Imhoff <gpg@kogakure.8shield.net>
2025-12-12 16:43:41 +0100

    fix: remove everyting XING

--- config/nvim/lua/plugins/gh-line.lua
+++ config/nvim/lua/plugins/gh-line.lua
@@ -2,8 +2,8 @@
 -- https://github.com/ruanyl/vim-gh-line
 -- <leader>gh (line) or <leader>gb (blame)
 return {
-  "ruanyl/vim-gh-line",
-  config = function()
-    vim.g.gh_github_domain = "source.xing.com"
-  end,
+	"ruanyl/vim-gh-line",
+	config = function()
+		vim.g.gh_github_domain = "github.com"
+	end,
 }
